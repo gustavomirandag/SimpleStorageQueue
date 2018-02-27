02c274007a19ef47909ba0d8432f391356d67dae
gustavomirandag <gustavomirandag@gmail.com>
2018-02-27 20:39:08 -0300

    Add project files.

---
+++ OrderConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure; // CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; //CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Queue; //CloudStorageQueue
+
+namespace OrderConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StorageAccount do Azure
+            var cloudStorageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            //CloudQueueClient
+            CloudQueueClient cloudQueueClient = cloudStorageAccount.CreateCloudQueueClient();
+
+            //CloudQueue vai fazer referência a uma fila específic
+            CloudQueue cloudQueue = cloudQueueClient.GetQueueReference("amazingqueue");
+
+            //Obtem a string que será adicionada a fila
+            Console.WriteLine("Digite o que quer adicionar na fila");
+            string strMessage = Console.ReadLine();
+        }
+    }
+}
---
+++ PackerConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure; // CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; //CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Queue; //CloudStorageQueue
+
+namespace PackerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StorageAccount do Azure
+            var cloudStorageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            //CloudQueueClient
+            CloudQueueClient cloudQueueClient = cloudStorageAccount.CreateCloudQueueClient();
+
+            //CloudQueue vai fazer referência a uma fila específic
+            CloudQueue cloudQueue = cloudQueueClient.GetQueueReference("amazingqueue");
+
+            //Pega a mensagem na frente da fila
+            CloudQueueMessage message = cloudQueue.GetMessage();
+            if (message != null)
+            {
+                Console.WriteLine(message.AsString);
+                cloudQueue.DeleteMessage(message);
+            }
+
+            //Apenas para não fechar a janela CMD
+            Console.ReadLine();
+        }
+    }
+}
